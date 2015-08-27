2e26e99479098c8d2b78520df9aac8720c8a0f83
Kilo3 <cubeintocube@gmail.com>
2015-08-27 11:40:16 +0900

    addresses 2gis, tables in content blocks, dialog pop up, email

--- src/Easy/MainBundle/Controller/FormController.php
+++ src/Easy/MainBundle/Controller/FormController.php
@@ -11,8 +11,18 @@ class FormController extends Controller
     public function indexAction(Request $request){
         $name = $request->request->get('name');
         $phone = $request->request->get('phone');
-        // отправка формы
-        echo "sended";die();
-        //return true;
+        
+        $to      = 'cubeintocube@gmail.com';
+        $subject = 'Запрос с сайта';
+        $message = "Имя: {$name}, Номер: {$phone}";
+        $headers = 'From: webmaster@e-a-s-y.com' . "\r\n" .
+                   'X-Mailer: PHP/' . phpversion();
+        //var_dump(mail($to, $subject, $message, $headers));die();
+        if(mail($to, $subject, $message, $headers)){
+            echo "sended";die();
+        }else{
+            echo "not sended";die();
+        }
+        
     }
 }
